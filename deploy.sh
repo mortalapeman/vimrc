e3225e5155fd8b35e4d40f15906ab01d0f1de9b9
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2013-01-27 00:38:47 -0600

    Added lein2 install to deployment script.

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,8 @@
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp .vimrc .gvimrc ~/
 vim +BundleInstall +qa
+if [ ! -d  ~/bin ]
+    mkdir ~/bin
+if
+wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
+chmod 755 ~/bin/lein

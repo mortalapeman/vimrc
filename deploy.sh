a503a8ec9672d49afc20935e76d2adb705f5d62a
Eric Pritchett <eric@eric-virtual-machine.(none)>
2013-05-12 11:21:23 -0500

    Modify cp command to include tmux config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp .vimrc .gvimrc ~/
+cp .tumx.conf .vimrc .gvimrc ~/
 if [ ! -d ~/.vim/bundle/vundle ]
   then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
@@ -14,4 +14,7 @@ if [ ! -f ~/bin/lein ]
   then
     wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
     chmod 755 ~/bin/lein
+    PATH=~/bin:$PATH
+    export PATH
+    echo "Don not forget to add ~/bin to your enviroment file."
 fi

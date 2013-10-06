f8aab79437fc963347f8a6e6342467e95dc08a1c
Eric Pritchett <eric.a.pritchett.che@gmail.com>
2013-10-05 23:33:02 -0500

    Change lein bin directory

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,15 @@ if [ ! -d ~/.vim/bundle/vundle ]
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
     vim +BundleInstall +qa
 fi
-if [ ! -d  ~/bin ]
+if [ ! -d  ~/.bin ]
   then
-    mkdir ~/bin
+    mkdir ~/.bin
 fi
-if [ ! -f ~/bin/lein ]
+if [ ! -f ~/.bin/lein ]
   then
     wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
-    chmod 755 ~/bin/lein
-    PATH=~/bin:$PATH
+    chmod 755 ~/.bin/lein
+    PATH=~/.bin:$PATH
     export PATH
-    echo "Don't not forget to add ~/bin to your enviroment file."
+    echo "Don't not forget to add ~/.bin to your enviroment file."
 fi
